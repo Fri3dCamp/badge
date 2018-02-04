3be4eb2117ec09c60a5701967d344fa0c014c384
Christophe VG <contact@christophe.vg>
2018-02-04 15:54:53 +0100

    added draft WiFi support

--- 2018/src/Fri3dBadge/examples/Fri3dBot/Fri3dBot.ino
+++ 2018/src/Fri3dBadge/examples/Fri3dBot/Fri3dBot.ino
@@ -1,13 +1,81 @@
 #include <Fri3dBadge.h>
 
+#include <WiFi.h>
+
+const char* ssid     = "NSAintel2C4team";
+const char* password = "100%wifi";
+
+WiFiServer server(80);
+
 void setup() {
 	Serial.begin(115200);
 	sta_neutraal();
   delay(3000);
+
+  setup_wifi();
+}
+
+void setup_wifi() {
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
 }
 
 void loop() {
   ontvang_en_verwerk();
+  handle_wifi();
+}
+
+void handle_wifi() {
+  WiFiClient client = server.available();
+  if (client) {
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.print("<a href=\"/n\">sta neutraal</a><br>\n");
+            client.print("<a href=\"/w\">wandel 3 stappen vooruit</a><br>\n");
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+        if (currentLine.endsWith("GET /n")) {
+          Serial.println("received cmd: n");
+          sta_neutraal();
+        }
+        if (currentLine.endsWith("GET /w")) {
+          wandel('v', 3);
+          Serial.println("received cmd: w");
+        }
+      }
+    }
+    client.stop();
+  }
 }
 
 // commando's die via monitor kunnen gegeven worden
